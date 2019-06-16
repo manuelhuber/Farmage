99dfe9a9c465e5fdbbdc6ef05c9c554ef58fdf2f
Manuel Huber <dev@manuel-huber.de>
2019-06-16 23:45:14 +0100

    added enemies

---
+++ Assets/Features/Enemies/EnemyScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Grimity.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Features.Enemies {
+public class EnemyScript : MonoBehaviour {
+    private GameObject[] _targets;
+    private GameObject _target;
+    private NavMeshAgent _navMeshAgent;
+    private CapsuleCollider _collider;
+
+    private void Start() {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _collider = GetComponent<CapsuleCollider>();
+    }
+
+    private void Update() {
+        if (_targets.Length == 0) return;
+        if (_target == null) {
+            _target = _targets.GetRandomElement();
+        }
+
+        _navMeshAgent.SetDestination(_target.transform.position);
+    }
+
+    public void setTargets(GameObject[] targets) {
+        _targets = targets;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("HQ")) {
+            Destroy(gameObject);
+        }
+    }
+}
+}
--- Assets/Features/Terrain/FarmageTerrain.cs
+++ Assets/Features/Terrain/FarmageTerrain.cs
@@ -1,62 +1,66 @@
 using System;
+using Grimity.Loops;
 using Grimity.Mesh;
 using Grimity.Rng;
 using UnityEngine;
 
 namespace Features.Terrain {
 public class FarmageTerrain : MonoBehaviour {
-    public Material material;
-    public int size = 250;
-    [Range(1, 10)] public int octaves = 6;
-    public float scale = 30f;
-    public float meshScale = 2.5f;
-    [Range(0, 1)] public float persistence = 0.4f;
-    [Range(1, 10)]public int lacunarity = 2;
-    public int seed = 8645153;
-    public AnimationCurve heightCurve;
-    public int heightAmplifier = 1;
-    private GameObject _chunk;
-    private MeshFilter _meshFilter;
-    private MeshCollider _collider;
-    private MeshRenderer _renderer;
+    public TerrainSettings settings;
+
+    public int yMax = 5;
+
+    private float RealWorldChunkLength => (settings.size - 1) * settings.meshScale;
 
     public void generateTerrain() {
-        if (_chunk == null) {
-            if (transform.childCount > 0 && transform.GetChild(0) != null) {
-                _chunk = transform.GetChild(0).gameObject;
-            } else {
-                _chunk = new GameObject();
-                _chunk.layer = 8;
-                _chunk.transform.parent = transform;
-                _meshFilter = _chunk.AddComponent<MeshFilter>();
-                _collider = _chunk.AddComponent<MeshCollider>();
-
-                _renderer = _chunk.AddComponent<MeshRenderer>();
-                _chunk.AddComponent<Rigidbody>().isKinematic = true;
-            }
+        foreach (var child in GetComponentsInChildren<Transform>()) {
+            if (child.gameObject == gameObject) continue;
+            UnityEditor.EditorApplication.delayCall += () => {
+                if (child == null) return;
+                DestroyImmediate(child.gameObject);
+            };
         }
 
-        var noise = Perlin.GeneratePerlinArray(size,
-                                               size,
-                                               octaves,
-                                               scale,
-                                               persistence,
-                                               lacunarity,
-                                               seed,
-                                               0,
-                                               0);
-        _renderer.material = material;
-        var mesh = MeshGenerator.GenerateMesh(
-            noise,
-            heightAmplifier,
-            heightCurve,
-            scale: meshScale).generateMesh();
-        _meshFilter.sharedMesh = mesh;
-        _collider.sharedMesh = mesh;
+        new Loop2D(yMax).loopX((x, y) => generateChunk(new Vector2Int(x, y)));
     }
 
+    public void generateChunk(Vector2Int pos) {
+        var chunk = new GameObject {name = "Terrain", layer = 8};
+        var xOffset = -(RealWorldChunkLength / 2) + RealWorldChunkLength * pos.x;
+        var yOffset = -(RealWorldChunkLength / 2) + RealWorldChunkLength * pos.y;
+        chunk.transform.position = new Vector3(xOffset, 0, yOffset);
+        chunk.transform.parent = transform;
+        var meshFilter = chunk.AddComponent<MeshFilter>();
+        var collider = chunk.AddComponent<MeshCollider>();
+        var renderer = chunk.AddComponent<MeshRenderer>();
+
+        chunk.AddComponent<Rigidbody>().isKinematic = true;
+
+        var noise = Perlin.GeneratePerlinArray(settings.size,
+                                               settings.size,
+                                               settings.octaves,
+                                               settings.scale,
+                                               settings.persistence,
+                                               settings.lacunarity,
+                                               settings.seed,
+                                               pos.x * (settings.size - 1),
+                                               pos.y * (settings.size - 1));
+
+        renderer.material = settings.material;
+        var mesh = MeshGenerator.GenerateMesh(
+            noise,
+            settings.heightAmplifier,
+            settings.heightCurve,
+            scale: settings.meshScale).generateMesh();
+        meshFilter.sharedMesh = mesh;
+        collider.sharedMesh = mesh;
+    }
+
+
     private void OnValidate() {
-        generateTerrain();
+        if (settings == null) return;
+        settings.OnValuesUpdated -= generateTerrain;
+        settings.OnValuesUpdated += generateTerrain;
     }
 }
 }
