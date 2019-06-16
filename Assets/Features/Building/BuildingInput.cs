01f4685aae138a795738947d85e7a1ee3f0258a2
Manuel Huber <dev@manuel-huber.de>
2019-06-16 10:26:00 +0100

    added building placement

--- Assets/Features/Building/BuildingInput.cs
+++ Assets/Features/Building/BuildingInput.cs
@@ -1,34 +1,83 @@
 using System;
-using UnityEngine.UIElements;
-using Button = UnityEngine.UI.Button;
+using System.Collections.Generic;
+using Features.Building;
+using Grimity.Cursor;
+using UnityEngine.UI;
 
 namespace UnityEngine {
 public class BuildingInput : MonoBehaviour {
-    public GameObject BuildingUIPrefab;
+    public List<BuildingMenuEntry> entries;
+    public LayerMask terrainLayer;
+
     private Canvas _canvas;
-    private GameObject _UiCore;
+    private GameObject _uiCore;
+    private bool _dragObject;
+    private Placeable _placeable;
+    private Camera _camera;
 
     private void OnEnable() {
-        _canvas = FindObjectOfType<Canvas>();
-        _UiCore = Instantiate(new GameObject(), _canvas.transform, false);
-        _UiCore.name = "Mouse Centric UI";
-        var button = Instantiate(BuildingUIPrefab, _UiCore.transform, false);
-        button.transform.localPosition = new Vector3(50, 0, 0);
-        var buttonClickedEvent = new Button.ButtonClickedEvent();
-        buttonClickedEvent.AddListener(() => { Debug.Log("Foo"); });
-        button.GetComponent<UI.Button>().onClick = buttonClickedEvent;
-
-
-        _UiCore.SetActive(false);
+        _camera = GetComponent<Camera>();
+        if (_camera == null) _camera = Camera.main;
+        InitUi();
     }
 
     private void Update() {
+        if (_dragObject) {
+            _placeable.gameObject.transform.position = MouseToTerrain().point - _placeable.lowerCenter;
+        }
+
         if (Input.GetKeyDown(KeyCode.B)) {
-            _UiCore.SetActive(true);
-            _UiCore.transform.position = Input.mousePosition;
+            _uiCore.SetActive(true);
+            _uiCore.transform.position = Input.mousePosition;
         } else if (Input.GetKeyDown(KeyCode.Escape)) {
-            _UiCore.SetActive(false);
+            _uiCore.SetActive(false);
+        }
+
+        if (Input.GetMouseButtonDown(0) && _dragObject) {
+            DetachFromCursor();
         }
     }
+
+    private void InitUi() {
+        _canvas = FindObjectOfType<Canvas>();
+        _uiCore = Instantiate(new GameObject(), _canvas.transform, false);
+        _uiCore.name = "Mouse Centric UI";
+        // TODO arrange them nicely
+        var offset = 50f;
+        foreach (var entry in entries) {
+            var button = Instantiate(entry.uiPrefab, _uiCore.transform, false);
+            button.transform.localPosition = new Vector3(offset, 0, 0);
+            offset += button.GetComponent<RectTransform>().rect.width;
+            var buttonClickedEvent = new Button.ButtonClickedEvent();
+            buttonClickedEvent.AddListener(() => { AttachToCursor(entry.buildingPrefab); });
+            button.GetComponent<Button>().onClick = buttonClickedEvent;
+        }
+
+        _uiCore.SetActive(false);
+    }
+
+    private void AttachToCursor(GameObject prefab) {
+        var building = Instantiate(prefab);
+        _placeable = building.AddComponent<Placeable>();
+        _placeable.terrainLayer = terrainLayer;
+        _uiCore.SetActive(false);
+        _dragObject = true;
+    }
+
+    private void DetachFromCursor() {
+        _dragObject = false;
+        _placeable.Place();
+    }
+
+    private RaycastHit MouseToTerrain() {
+        CursorUtil.GetCursorLocation(out RaycastHit terrainHit, terrainLayer, _camera, debug: true);
+        return terrainHit;
+    }
+}
+
+[Serializable]
+public struct BuildingMenuEntry {
+    public GameObject uiPrefab;
+    public GameObject buildingPrefab;
 }
 }
---
+++ Assets/Features/Building/Placeable.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Grimity.GameObjects;
+using UnityEngine;
+
+namespace Features.Building {
+public class Placeable : MonoBehaviour {
+    public Vector3 lowerCenter;
+    public LayerMask terrainLayer;
+
+    private MeshFilter _filter;
+    private BoxCollider _floorChecker;
+    private List<Vector3> _terrainVertices;
+    private List<Vector3> _terrainVerticesWorldSpace;
+    private Mesh _terrainMesh;
+    private MeshCollider _terrainCollider;
+    private GameObject _terrain;
+    private List<Tuple<int, Vector3>> _collisionVertices = new List<Tuple<int, Vector3>>();
+
+    private void Start() {
+        _filter = GetComponent<MeshFilter>();
+        var o = gameObject;
+        _floorChecker = o.AddComponent<BoxCollider>();
+        var bounds = _filter.sharedMesh.bounds;
+        // TODO remove magic numbers
+        _floorChecker.center = new Vector3(0, -0.5f, 0);
+        _floorChecker.isTrigger = true;
+        var floorCheckerSize = bounds.size;
+        floorCheckerSize.y = 20;
+        floorCheckerSize.x = 2;
+        floorCheckerSize.z = 2;
+        _floorChecker.size = floorCheckerSize;
+        lowerCenter = Geometry.LowerCenter(o);
+    }
+
+    public void Place() {
+        var collisionVertices = CollisionVertices();
+
+        var avg = collisionVertices.Select(tuple => tuple.Item2.y)
+                      .Aggregate((sum, value) => sum + value) /
+                  collisionVertices.Count;
+        foreach (var (index, pos) in collisionVertices) {
+            _terrainVertices.RemoveAt(index);
+            _terrainVertices.Insert(index, new Vector3(pos.x, avg, pos.z));
+        }
+
+
+        var newHeight = Math.Abs(lowerCenter.y) + avg;
+
+        transform.SetY(newHeight);
+        _terrainMesh.SetVertices(_terrainVertices);
+        _terrainCollider.sharedMesh = null;
+        _terrainCollider.sharedMesh = _terrainMesh;
+
+        Destroy(this);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (terrainLayer != (terrainLayer | (1 << other.gameObject.layer))) return;
+        _terrainMesh = other.GetComponent<MeshFilter>().sharedMesh;
+        _terrainCollider = other.GetComponent<MeshCollider>();
+        _terrain = other.gameObject;
+        _terrainVertices = _terrainMesh.vertices.ToList();
+        var localToWorld = _terrain.transform.localToWorldMatrix;
+        _terrainVerticesWorldSpace =
+            _terrainVertices.Select(vector3 => localToWorld.MultiplyPoint3x4(vector3)).ToList();
+    }
+
+    private void OnTriggerStay(Collider other) {
+        var collisionVertices = CollisionVertices();
+        if (collisionVertices.Count == 0) return;
+        var heights = collisionVertices.Select(tuple => tuple.Item2.y).ToArray();
+        var dif = heights.Max() - heights.Min();
+        // TODO implement "not placeable" feature
+        if (dif > 2) {
+            Debug.Log("Bad place");
+        }
+    }
+
+    private List<Tuple<int, Vector3>> CollisionVertices() {
+        _collisionVertices.Clear();
+        if (_terrainVertices == null) return _collisionVertices;
+        for (var i = 0; i < _terrainVertices.Count; i++) {
+            if (_floorChecker.bounds.Contains(_terrainVerticesWorldSpace[i])) {
+                _collisionVertices.Add(new Tuple<int, Vector3>(i, _terrainVertices[i]));
+            }
+        }
+
+        return _collisionVertices;
+    }
+}
+}
