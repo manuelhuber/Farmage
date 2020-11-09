9de1d5cc9e7c43e2949af4e8fc5221e29b40ffd1
Grim <dev@manuel-huber.de>
2020-11-09 14:55:52 +0000

    Updated enemy behaviour

--- Assets/Features/Units/Enemies/EnemyScript.cs
+++ Assets/Features/Units/Enemies/EnemyScript.cs
@@ -1,112 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Features.Common;
 using Features.Health;
 using Features.Save;
-using Features.Time;
 using Features.Units.Common;
-using Grimity.Actions;
-using Grimity.Collections;
-using JetBrains.Annotations;
+using Grimity.Data;
 using UnityEngine;
 
 namespace Features.Units.Enemies {
+[RequireComponent(typeof(AdvancedMovementController))]
+[RequireComponent(typeof(Mortal))]
 public class EnemyScript : MonoBehaviour, ISavableComponent<EnemyData> {
-    public float attackSpeed;
-    public int damage = 5;
-    private PeriodicalAction _attack;
-    private MovementAgent _movementAgent;
-    private List<Mortal> _targets = new List<Mortal>();
-    private GameTime _time;
-    [CanBeNull] private Mortal _victim;
-
-
-    private void Awake() {
-        _time = GameTime.Instance;
-        _movementAgent = GetComponent<MovementAgent>();
-        _attack = gameObject.AddComponent<PeriodicalAction>();
-        _attack.interval = attackSpeed;
-        _attack.getTime = () => _time.Time;
-        _attack.action = () => {
-            if (_victim != null) _victim.TakeDamage(new Damage {Amount = damage});
-            return true;
-        };
-        var rangeCollider = RangeCollider.AddTo(gameObject, 1.2f);
-        rangeCollider.OnEnter(OnEnterRange);
-        rangeCollider.OnExit(OnExitRange);
-    }
-
-    private void Start() {
-        FindNewTarget();
-    }
-
-    private void OnEnterRange(Collider other) {
-        var destructible = other.gameObject.GetComponent<Mortal>();
-        if (destructible == null || destructible.Team == Team.Aliens) return;
-        transform.LookAt(destructible.transform);
-        _movementAgent.IsStopped = true;
-        SetTarget(destructible);
-    }
-
-    private void OnExitRange(Collider other) {
-        var victim = _victim;
-        if (victim == null) return;
-        if (other.gameObject != victim.gameObject) return;
-        StopAttack();
-        _movementAgent.SetDestination(victim.transform.position, true);
-    }
-
-    public void SetTargets(List<Mortal> targets) {
-        _targets = targets;
-        foreach (var target in _targets) {
-            target.onDeath.AddListener(() => _targets.Remove(target));
+    public Mortal DefaultTarget {
+        set {
+            _defaultTarget = value.AsOptional();
+            UpdateTarget();
         }
     }
 
-    private void SetTarget(Mortal destructible) {
-        _victim = destructible;
-        _attack.IsRunning = true;
-        destructible.onDeath.AddListener(() => {
-            _victim = null;
-            StopAttack();
-        });
+    private Optional<Mortal> _threateningTarget = Optional<Mortal>.NoValue();
+    private AdvancedMovementController _movementController;
+    private Optional<Mortal> _defaultTarget;
+
+
+    private void Awake() {
+        _movementController = GetComponent<AdvancedMovementController>();
+        GetComponent<Mortal>().OnDamageInterceptor.Add(OnDamage);
     }
 
-    private void StopAttack() {
-        _attack.IsRunning = false;
-        _movementAgent.IsStopped = false;
-        FindNewTarget();
+
+    private Damage OnDamage(Damage damage) {
+        if (!_threateningTarget.HasValue && damage.Source != null) {
+            var mortal = damage.Source.GetComponent<Mortal>();
+            if (mortal != null) {
+                _threateningTarget = mortal.AsOptional();
+                mortal.onDeath.AddListener(() => {
+                    _threateningTarget = Optional<Mortal>.NoValue();
+                    UpdateTarget();
+                });
+                UpdateTarget();
+            }
+        }
+
+        return damage;
     }
 
-    private void FindNewTarget() {
-        if (_targets.Count == 0) return;
-        if (_targets.All(t => t == null)) return;
-        var target = _targets.GetRandomElement();
-        _movementAgent.SetDestination(target.transform.position, true);
+    private void UpdateTarget() {
+        if (_threateningTarget.HasValue) {
+            _movementController.ChaseTarget(_threateningTarget.Value.transform);
+        } else if (_defaultTarget.HasValue) {
+            _movementController.AttackMoveTo(_defaultTarget.Value.transform.position);
+        }
     }
+
 
     #region Save
 
     public string SaveKey => "EnemyController";
 
     public EnemyData Save() {
-        var victim = _victim;
-        return new EnemyData {
-            target = victim == null ? "" : victim.getSaveID(),
-            targets = _targets.Select(mortal => mortal.getSaveID()).ToArray(),
-            nextAttack = _attack.NextExecution
-        };
+        return new EnemyData();
     }
 
     public void Load(EnemyData data, IReadOnlyDictionary<string, GameObject> objects) {
-        _attack.SetNextExecution(data.nextAttack);
-        _targets = data.targets.Select(t => objects[t].GetComponent<Mortal>()).ToList();
-
-        var target = objects.getBySaveID(data.target);
-        if (target != null) {
-            SetTarget(target.GetComponent<Mortal>());
-        }
     }
 
     #endregion
@@ -114,8 +69,5 @@ public class EnemyScript : MonoBehaviour, ISavableComponent<EnemyData> {
 
 [Serializable]
 public struct EnemyData {
-    public string target;
-    public string[] targets;
-    public float nextAttack;
 }
 }
--- Assets/Features/Units/Enemies/Spawner.cs
+++ Assets/Features/Units/Enemies/Spawner.cs
@@ -1,42 +1,53 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using Features.Health;
 using Features.Time;
 using Grimity.Collections;
+using Grimity.ScriptableObject;
+using MonKey.Extensions;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Features.Units.Enemies {
 public class Spawner : MonoBehaviour {
+    public RuntimeGameObjectSet allFarmerBuildings;
     public GameObject enemyPrefab;
 
     public GameObject[] spawnPoints;
     public float waveInterval = 5f;
-    private GameObject[] _hqs;
+
+    [InfoBox("Min/Max number of enemies spawned per wave")]
+    public RangeInt SpawnCount = new RangeInt(1, 1);
 
     private float _lastSpawn;
     private GameTime _time;
-
-    [InfoBox("Min/Max number of enemies spawned per wave")]
-    public RangeInt spawnCount = new RangeInt(1, 1);
+    private List<Mortal> _availableBuildings = new List<Mortal>();
 
     private void Awake() {
         _time = GameTime.Instance;
+        allFarmerBuildings.OnChange += OnBuildingChange;
+    }
+
+    private void OnBuildingChange(ReadOnlyCollection<GameObject> items) {
+        _availableBuildings = items.Select(go => go.GetComponent<Mortal>()).ToList();
     }
 
     private void Update() {
         if (_time.Time - _lastSpawn < waveInterval) return;
-        _hqs = GameObject.FindGameObjectsWithTag("HQ");
-        _lastSpawn = _time.Time;
-        var count = Random.Range(spawnCount.start, spawnCount.end);
+        if (_availableBuildings.IsEmpty()) return;
+        var target = _availableBuildings.GetRandomElement();
+        var spawnLocation = spawnPoints.GetRandomElement().transform;
+        var count = Random.Range(SpawnCount.start, SpawnCount.end);
         var offset = 0;
         for (var i = 0; i < count; i++) {
-            var spawnLocation = spawnPoints.GetRandomElement().transform;
             var offsetLocation = spawnLocation.position + new Vector3(0, 0, offset);
             var enemy = Instantiate(enemyPrefab, offsetLocation, spawnLocation.rotation, transform);
-            enemy.GetComponent<EnemyScript>()
-                .SetTargets(_hqs.Select(go => go.GetComponent<Mortal>()).ToList());
+            enemy.GetComponent<EnemyScript>().DefaultTarget = target;
             offset += 10;
         }
+
+        _lastSpawn = _time.Time;
     }
 }
 }
