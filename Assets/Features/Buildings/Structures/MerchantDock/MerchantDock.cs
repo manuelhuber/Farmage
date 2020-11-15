6cce4b3e9390ebc5f1c9462109b06f0904f2cd55
Grim <dev@manuel-huber.de>
2020-11-15 21:03:47 +0000

    Added merchant shop

--- Assets/Features/Buildings/Structures/MerchantDock/MerchantDock.cs
+++ Assets/Features/Buildings/Structures/MerchantDock/MerchantDock.cs
@@ -1,33 +1,38 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
-using Features.Delivery;
+using Features.Buildings.BuildMenu;
+using Features.Buildings.UI;
 using Features.Resources;
-using Features.Tasks;
 using Features.Time;
 using Features.Ui.Actions;
+using Grimity.Collections;
 using Grimity.Data;
 using UnityEngine;
 
 namespace Features.Buildings.Structures.MerchantDock {
-public class MerchantDock : MonoBehaviour, IDeliveryAcceptor, IHasActions {
+public class MerchantDock : MonoBehaviour, IHasActions {
     public int visitIntervalsInSeconds;
-    public Resource[] SellableResources;
+    public int stayDurationInSeconds;
+    public int itemSlots;
 
-    public bool TradeInProgress { get; private set; }
+    [SerializeField] private MerchantInventory inventory;
 
     private readonly Observable<ActionEntryData[]>
         _actions = new Observable<ActionEntryData[]>(new ActionEntryData[0]);
 
+    private readonly List<PurchaseCost> _rolledItems = new List<PurchaseCost>();
+    private BuildingManager _buildingManager;
+
     private GameTime _gameTime;
     private float _nextTradeTimestamp;
     private ResourceManager _resourceManager;
-    private TaskManager _taskManager;
+    private bool _tradeInProgress;
 
     private void Awake() {
-        _taskManager = TaskManager.Instance;
         _gameTime = GameTime.Instance;
         _resourceManager = ResourceManager.Instance;
-        _actions.Set(SellableResources.Select(CreateActionEntry).ToArray());
+        _buildingManager = BuildingManager.Instance;
+        _resourceManager.Have.OnChange(cost => UpdateActionEntries());
     }
 
     private void Start() {
@@ -35,52 +40,68 @@ public class MerchantDock : MonoBehaviour, IDeliveryAcceptor, IHasActions {
     }
 
     private void Update() {
-        if (TradeInProgress) return;
-        if (_gameTime.Time >= _nextTradeTimestamp) {
+        if (_gameTime.Time < _nextTradeTimestamp) return;
+        if (_tradeInProgress) {
+            FinishTrade();
+        } else {
             InitTrade();
         }
     }
 
-
-    public bool AcceptDelivery(GameObject goods) {
-        var resourceObject = goods.GetComponent<ResourceObject>();
-        var price = resourceObject.resource.worth;
-        _resourceManager.Add(price);
-        Destroy(goods);
-        return true;
-    }
-
-    public Grimity.Data.IObservable<ActionEntryData[]> GetActions() {
+    public IObservable<ActionEntryData[]> GetActions() {
         return _actions;
     }
 
-    private ActionEntryData CreateActionEntry(Resource itemType) {
-        return new ActionEntryData {
-            Active = true,
-            OnSelect = EnqueueTask(itemType)
-        };
-    }
-
-    private Action EnqueueTask(Resource resource) {
-        return () => {
-            var item = _resourceManager.ReserveItem(resource);
-            if (!item.HasValue) return;
-            var (storable, storage) = item.Value;
-            _taskManager.Enqueue(new DeliveryTask(
-                storable.gameObject,
-                storage.gameObject.AsOptional(),
-                gameObject.AsOptional()
-            ));
-        };
+    private void FinishTrade() {
+        Debug.Log("TRADE ENDED");
+        _tradeInProgress = false;
+        _rolledItems.Clear();
+        UpdateActionEntries();
+        _nextTradeTimestamp = _gameTime.Time + visitIntervalsInSeconds;
     }
 
     private void InitTrade() {
-        TradeInProgress = true;
+        Debug.Log("TRADE STARTED");
+        _tradeInProgress = true;
+        _nextTradeTimestamp = _gameTime.Time + stayDurationInSeconds;
+        CreateActionsMenu();
+        UpdateActionEntries();
     }
 
-    private void FinishTrade() {
-        TradeInProgress = false;
-        _nextTradeTimestamp = _gameTime.Time + visitIntervalsInSeconds;
+    private void CreateActionsMenu() {
+        var lootTable = new List<PurchaseCost>();
+        foreach (var entry in inventory.costs) {
+            for (var i = 0; i < entry.rollWeight; i++) {
+                lootTable.Add(entry);
+            }
+        }
+
+        _rolledItems.Clear();
+        for (var i = 0; i < itemSlots; i++) {
+            _rolledItems.Add(lootTable.GetRandomElement());
+        }
+    }
+
+    private void UpdateActionEntries() {
+        _actions.Set(_rolledItems.Select(CreateActionEntry).ToArray());
+    }
+
+    private ActionEntryData CreateActionEntry(PurchaseCost arg) {
+        if (!(arg.item is BuildingMenuEntry buildingMenuEntry)) return null;
+
+        void BuyBuilding() {
+            if (_resourceManager.Pay(arg.cost)) {
+                _buildingManager.AddBuildingOption(buildingMenuEntry);
+                _rolledItems.Remove(arg);
+                UpdateActionEntries();
+            }
+        }
+
+        return new ActionEntryData {
+            Image = buildingMenuEntry.image,
+            Active = _resourceManager.CanBePayed(arg.cost),
+            OnSelect = BuyBuilding
+        };
     }
 }
 }
